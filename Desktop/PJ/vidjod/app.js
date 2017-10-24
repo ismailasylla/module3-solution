b0fe1f2ac4f3117e338316aa742c892692f68fd2
ismaila sylla <syldox@gmail.com>
2017-10-25 00:13:36 +0530

    making small changes / end

--- Desktop/PJ/vidjod/app.js
+++ Desktop/PJ/vidjod/app.js
@@ -1,62 +1,61 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const methodOverride = require('method-override')
-const hbs = require('hbs');
-const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const flash = require('connect-flash');
-const session = require('express-session')
+const mongoose = require('mongoose');
 
 const app = express();
 
-// loads routes
+// Load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
 
-//passport config 
+// Passport Config
 require('./config/passport')(passport);
 
-
-//Map global promise -get rid of warning
+// Map global promise - get rid of warning
 mongoose.Promise = global.Promise;
-
-//connect to mongoose
-mongoose.connect('mongodb://localhost:27017/webjob-dev', {
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
         useMongoClient: true
     })
-    .then(() => console.log('MongoDB Connected'))
+    .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
-//handlebars Middlewares 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
-//Body parser Middleware
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-//static folder 
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Method override Middleware
+// Method override middleware
 app.use(methodOverride('_method'));
 
-//Express Session Middleware
+// Express session midleware
 app.use(session({
     secret: 'secret',
     resave: true,
     saveUninitialized: true
 }));
 
-//passport middleware
+// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(flash());
 
-//Global variables
+// Global variables
 app.use(function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
@@ -65,31 +64,26 @@ app.use(function(req, res, next) {
     next();
 });
 
-
-//index Route
+// Index Route
 app.get('/', (req, res) => {
-    const title = "Welcome";
+    const title = 'Welcome';
     res.render('index', {
         title: title
     });
 });
 
+// About Route
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
 
-
-
-
-//Use routes
+// Use routes
 app.use('/ideas', ideas);
 app.use('/users', users);
-
-
 
 const port = 3000;
 
 app.listen(port, () => {
-    console.log(`Server Started on port ${port}`);
+    console.log(`Server started on port ${port}`);
 });
--- Desktop/PJ/vidjod/routes/ideas.js
+++ Desktop/PJ/vidjod/routes/ideas.js
@@ -3,16 +3,11 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const { ensureAuthenticated } = require('../helpers/auth');
 
-
-
-
-module.exports = router;
-
-//load the idea model
+// Load Idea Model
 require('../models/Idea');
 const Idea = mongoose.model('ideas');
 
-//idea index page 
+// Idea Index Page
 router.get('/', ensureAuthenticated, (req, res) => {
     Idea.find({ user: req.user.id })
         .sort({ date: 'desc' })
@@ -23,22 +18,20 @@ router.get('/', ensureAuthenticated, (req, res) => {
         });
 });
 
-//add idea form
+// Add Idea Form
 router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('ideas/add');
 });
 
-
-//edit idea form
+// Edit Idea Form
 router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
             _id: req.params.id
         })
         .then(idea => {
             if (idea.user != req.user.id) {
-                req.flash('error_msg', 'You are Not Autorized');
-                res.redirect('/ideas')
-
+                req.flash('error_msg', 'Not Authorized');
+                res.redirect('/ideas');
             } else {
                 res.render('ideas/edit', {
                     idea: idea
@@ -48,28 +41,23 @@ router.get('/edit/:id', ensureAuthenticated, (req, res) => {
         });
 });
 
-
-
-//process form
+// Process Form
 router.post('/', ensureAuthenticated, (req, res) => {
     let errors = [];
 
     if (!req.body.title) {
         errors.push({ text: 'Please add a title' });
-
     }
     if (!req.body.details) {
-        errors.push({ text: 'Please add a details ' });
-
+        errors.push({ text: 'Please add some details' });
     }
 
     if (errors.length > 0) {
-        res.render('ideas/add', {
+        res.render('/add', {
             errors: errors,
             title: req.body.title,
             details: req.body.details
         });
-
     } else {
         const newUser = {
             title: req.body.title,
@@ -79,36 +67,37 @@ router.post('/', ensureAuthenticated, (req, res) => {
         new Idea(newUser)
             .save()
             .then(idea => {
-                req.flash('success_msg', 'New Note Added');
+                req.flash('success_msg', 'Video idea added');
                 res.redirect('/ideas');
             })
     }
-
 });
 
-//Edit form process and updates
+// Edit Form process
 router.put('/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
             _id: req.params.id
         })
         .then(idea => {
-            // new values 
+            // new values
             idea.title = req.body.title;
             idea.details = req.body.details;
 
             idea.save()
                 .then(idea => {
+                    req.flash('success_msg', 'Video idea updated');
                     res.redirect('/ideas');
                 })
-
         });
 });
 
-//delete idea 
+// Delete Idea
 router.delete('/:id', ensureAuthenticated, (req, res) => {
     Idea.remove({ _id: req.params.id })
         .then(() => {
-            req.flash('success_msg', 'Idea removed');
+            req.flash('success_msg', 'Video idea removed');
             res.redirect('/ideas');
         });
 });
+
+module.exports = router;
